fd02562fd8016caa1c977ddb512542eb92a86dc8
Guy Freeman <git@gfrm.in>
2015-10-24 14:16:20 +0800

    R code for adding email addresses from hkgov teldir

---
+++ access.R
@@ -0,0 +1,17 @@
+library(jsonlite)
+library(data.table)
+library(stringr)
+
+ati <- data.table(fromJSON("ati.json"))
+
+ati[,subdepartment := str_replace(subdepartment, "AAA", "")]
+ati[,subdepartment := str_replace(subdepartment, department, "")]
+ati[,fax := str_replace(fax, "\\(.*?\\) ", "")]
+ati[,tel := str_replace(tel, "\\(.*?\\) ", "")]
+# ati[,
+
+teldir <- fread("https://hkdata.dataguru.hk/assets/teldir.csv")
+
+setkey(ati, tel)
+setkey(teldir, tel)
+teldirati <- teldir[,list(tel,email)][ati]
