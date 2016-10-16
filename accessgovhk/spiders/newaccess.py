c42495a2ec8c46ec88fa88005a174f5ae2f89cf3
Guy Freeman <git@gfrm.in>
2016-10-16 23:23:02 +0800

    Get all pages properly

--- accessgovhk/spiders/newaccess.py
+++ accessgovhk/spiders/newaccess.py
@@ -14,7 +14,7 @@ class NewAccessSpider(scrapy.Spider):
 
     def parse(self, response):
         departments = response.css('#content .clearfix a')
-        departmentlinks = [link for link in departments.xpath('@href').extract() if link != u'javascript:void(0);'] # because there are pointless void(0) links....!?!?
+        departmentlinks = [link.strip() for link in departments.xpath('@href').extract() if link != u'javascript:void(0);'] # because there are pointless void(0) links....!?!?
         for href in departmentlinks:
             url = response.urljoin(href)
             request = scrapy.Request(url, callback=self.parsedeptpage)
@@ -36,7 +36,7 @@ class NewAccessSpider(scrapy.Spider):
             item['address'] = ", ".join([x.strip() for x in Selector(text = departmentinfo[4]).xpath('//text()').extract()]).replace(",,", ",")
             item['homepage'] = homepage
             yield item
-        elif not response.css(".tabController"):
+        else:
             departmentinfo = response.css('p').extract()
             for sectionid, subdepartmentsection in enumerate(response.css("h3::text").extract()):
                 subdepartmentinfo = departmentinfo[(sectionid*5):((sectionid+1)*5)]
@@ -49,25 +49,9 @@ class NewAccessSpider(scrapy.Spider):
                 item['address'] = ", ".join([x.strip() for x in Selector(text = subdepartmentinfo[4]).xpath('//text()').extract()]).replace(",,", ",")
                 item['homepage'] = homepage
                 yield item
-        else:
-            tabpagelinks = response.css(".tab a::attr(href)").extract()
-            for href in tabpagelinks:
-                url = response.urljoin(href)
-                request = scrapy.Request(url, callback=self.parsetabpage)
-                yield request
-
-    def parsetabpage(self, response):
-        departmentname = " ".join(response.css("h2::text").extract_first().split())
-        homepage = response.css(".btn_website::attr(href)").extract_first()
-        departmentinfo = response.css('p').extract()
-        for sectionid, subdepartmentsection in enumerate(response.css("h3::text").extract()):
-            subdepartmentinfo = departmentinfo[(sectionid*5):((sectionid+1)*5)]
-            item = DepartmentItem()
-            item['department'] = departmentname + "\n" + subdepartmentsection.strip()
-            item['name'] = str.join("", Selector(text = subdepartmentinfo[0]).xpath('//text()').extract()).strip()
-            item['tel'] = str.join("", Selector(text = subdepartmentinfo[1]).xpath('//text()').extract()).strip()
-            item['fax'] = str.join("", Selector(text = subdepartmentinfo[2]).xpath('//text()').extract()).strip()
-            item['email'] = str.join("", Selector(text = subdepartmentinfo[3]).xpath('//text()').extract()).strip()
-            item['address'] = ", ".join([x.strip() for x in Selector(text = subdepartmentinfo[4]).xpath('//text()').extract()]).replace(",,", ",")
-            item['homepage'] = homepage
-            yield item
+            if response.css(".tabController"):
+                tabpagelinks = response.css(".tab a::attr(href)").extract()
+                for href in tabpagelinks:
+                    url = response.urljoin(href)
+                    request = scrapy.Request(url, callback=self.parsedeptpage)
+                    yield request
